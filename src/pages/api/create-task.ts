adeea6498de1a3fa8b6783a2837b26404cebf519
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-02-18 07:30:55 -0800

    create task api

--- src/pages/api/create-task.ts
+++ src/pages/api/create-task.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase-admin/firestore";
 import { FieldValue } from "firebase-admin/firestore";
 
 type Data = {
-  user: any | null;
+  task: any | null;
   error?: string;
 };
 
@@ -13,34 +13,31 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const user = await getUserFromCookies({ req, includeToken: true });
-
-  if (!user) {
-    return res.status(401).json({ error: "Unauthorized", user: null });
-  }
-
   const db = getFirestore();
-
-  console.log("REQ BODY", req.body);
 
   const userDoc = await db
     .collection(req.body.city)
     .doc(req.body.user?.id)
+    .get();
+  const userData = userDoc.data();
+
+  if (!userData || userData.role !== "admin") {
+    return res.status(401).json({ error: "Unauthorized", task: null });
+  }
+
+  console.log("REQ BODY", req.body);
+
+  const task = await db
+    .collection(req.body.city)
+    .doc(req.body.user?.id)
+    .collection("tasks")
+    .doc(req.body.taskId)
     .set({
-      userId: req.body.user?.id,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email || "",
-      countryCode: req.body.countryCode || "",
-      phone: req.body.phoneNumber || "",
-      address: req.body.address || "",
-      shippingAddress: req.body.shippingAddress || "",
-      dateOfBirth: req.body.dateOfBirth || "",
-      bio: "",
-      pfpURL: "",
-      role: "user",
+      description: req.body.description,
       createdAt: FieldValue.serverTimestamp(),
       updatedAt: FieldValue.serverTimestamp(),
+      amount: req.body.amount,
     });
 
-  res.status(200).json({ user: userDoc });
+  res.status(200).json({ task });
 }
