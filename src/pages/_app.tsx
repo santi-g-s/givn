1a08e21941d6dcdf6caa299ecc75847d46c8a8ba
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-02-18 09:19:11 -0800

    add login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import initAuth from "@/initAuth";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  initAuth();
+  return <Component {...pageProps} />;
 }
