d07826d67cf2821855756d10150976d65e3eba3e
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-02-17 22:43:28 -0800

    init firebase

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseApp =
+  getApps().length === 0
+    ? initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+      })
+    : getApp();
+
+export const firestore = getFirestore();
+export const auth = getAuth();
+export default firebaseApp;
---
+++ src/initAuth.ts
@@ -0,0 +1,62 @@
+import "./firebase";
+import { init } from "next-firebase-auth";
+
+const prod = process.env.NODE_ENV === "production";
+
+const initAuth = () => {
+  init({
+    authPageURL: "/accounts/login",
+    appPageURL: "/profile",
+    loginAPIEndpoint: "/api/login", // required
+    logoutAPIEndpoint: "/api/logout", // required
+    onLoginRequestError: (err) => {
+      console.error(err);
+    },
+    onLogoutRequestError: (err) => {
+      console.error(err);
+    },
+    firebaseAuthEmulatorHost: process.env.FIREBASE_AUTH_EMULATOR_HOST,
+    firebaseAdminInitConfig: {
+      credential: {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
+        // The private key must not be accessible on the client side.
+        privateKey: process.env.FIREBASE_PRIVATE_KEY || "",
+      },
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "",
+    },
+    // Use application default credentials (takes precedence over fireaseAdminInitConfig if set)
+    // useFirebaseAdminDefaultCredential: true,
+    firebaseClientInitConfig: {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "", // required
+      // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      // databaseURL: "https://my-example-app.firebaseio.com",
+      // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    },
+    cookies: {
+      name: "TEMP", // required
+      // Keys are required unless you set `signed` to `false`.
+      // The keys cannot be accessible on the client side.
+      keys: [
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
+      ],
+      httpOnly: true,
+      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+      overwrite: true,
+      path: "/",
+      sameSite: "strict",
+      secure: prod, // set this to false in local (non-HTTPS) development
+      signed: false,
+    },
+    onVerifyTokenError: (err) => {
+      console.error(err);
+    },
+    onTokenRefreshError: (err) => {
+      console.error(err);
+    },
+  });
+};
+
+export default initAuth;
