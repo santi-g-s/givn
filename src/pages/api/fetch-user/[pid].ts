73460204d6da33f9ad647f22b73b9f2cbea6f130
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-02-18 17:58:04 -0800

    dynamic profile page

---
+++ src/pages/api/fetch-user/[pid].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getFirestore } from "firebase-admin/firestore";
+
+type Data = {
+  user: any;
+  error?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { pid } = req.query;
+
+  console.log("pid", pid);
+  const db = getFirestore();
+
+  const userRef = db
+    .collection("city")
+    .doc("875491")
+    .collection("users")
+    .doc((pid as string) || "");
+  const userDoc = await userRef.get();
+  const userData = userDoc.data();
+
+  res.status(200).json({
+    user: userData,
+  });
+}
