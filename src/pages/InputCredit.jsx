c63f1346883a0ded5e3d6efba2f978c67c93a67b
Jerry Zhou <8799269+jzhou891@users.noreply.github.com>
2023-02-18 17:02:04 -0800

    update profile page

--- src/pages/InputCredit.jsx
+++ src/pages/InputCredit.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import {styles} from '../styles/InputCreditStyle.module.css';
-import Script from 'next/script'
-
-// styles for VGS Collect fields
-// const styles = {
-//   fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI"',
-// }
+import React, { useEffect, useState } from "react";
+import { Input } from "@mantine/core";
+import styles from "../styles/InputCreditStyle.module.css";
 
 const CollectForm = () => {
   const [form, setForm] = useState({});
   const [isLoaded, scriptLoaded] = useState(false);
+  const [amount, setAmount] = useState("0.00");
 
   // script loading
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = 'https://js.verygoodvault.com/vgs-collect/2.18.2/vgs-collect.js';
+    script.src =
+      "https://js.verygoodvault.com/vgs-collect/2.18.2/vgs-collect.js";
     script.async = true;
     script.onload = () => scriptLoaded(true);
     document.body.appendChild(script);
@@ -23,56 +20,51 @@ const CollectForm = () => {
   // VGS Collect initialization
   useEffect(() => {
     if (isLoaded) {
-      const vgsForm = window.VGSCollect.create('tntsfeqzp4a', 'sandbox', (state) => {
-        console.log(state);
-      });
+      const vgsForm = window.VGSCollect.create(
+        "tntsfeqzp4a",
+        "sandbox",
+        (state) => {}
+      );
       setForm(vgsForm);
 
-      vgsForm.field('#cc-holder', {
-        type: 'text',
-        name: 'card_holder',
-        placeholder: 'Card holder',
-        validations: ['required'],
+      vgsForm.field("#cc-holder", {
+        type: "text",
+        name: "card_holder",
+        placeholder: "Card Holder",
+        validations: ["required"],
         css: styles,
       });
 
-      vgsForm.field('#cc-number', {
-        type: 'card-number',
-        name: 'card_number',
-        successColor: '#4F8A10',
-        errorColor: '#D8000C',
-        placeholder: 'Card number',
+      vgsForm.field("#cc-number", {
+        type: "card-number",
+        name: "card_number",
+        successColor: "#4F8A10",
+        errorColor: "#D8000C",
+        placeholder: "Card Number",
         showCardIcon: true,
-        validations: ['required', 'validCardNumber'],
+        validations: ["required", "validCardNumber"],
         css: styles,
       });
 
-      vgsForm.field('#cc-amount-money', {
-        type: 'text',
-        name: 'amount_money',
-        successColor: '#4F8A10',
-        errorColor: '#D8000C',
-        placeholder: 'Amount of money',
+      vgsForm.field("#cc-cvc", {
+        type: "card-security-code",
+        name: "card_cvc",
+        successColor: "#4F8A10",
+        errorColor: "#D8000C",
+        placeholder: "- - -",
+        maxLength: 3,
+        validations: ["required", "validCardSecurityCode"],
         css: styles,
+        showCardIcon: true,
       });
 
-      vgsForm.field('#cc-cvc', {
-        type: 'card-security-code',
-        name: 'card_cvc',
-        successColor: '#4F8A10',
-        errorColor: '#D8000C',
-        placeholder: 'CVC',
-        validations: ['required', 'validCardSecurityCode'],
-        css: styles,
-      });
-
-      vgsForm.field('#cc-expiration-date', {
-        type: 'card-expiration-date',
-        name: 'card_exp',
-        successColor: '#4F8A10',
-        errorColor: '#D8000C',
-        placeholder: 'MM / YY',
-        validations: ['required', 'validCardExpirationDate'],
+      vgsForm.field("#cc-expiration-date", {
+        type: "card-expiration-date",
+        name: "card_exp",
+        successColor: "#4F8A10",
+        errorColor: "#D8000C",
+        placeholder: "MM / YY",
+        validations: ["required", "validCardExpirationDate"],
         css: styles,
       });
     }
@@ -81,8 +73,9 @@ const CollectForm = () => {
   // VGS Collect form submission
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("submit");
     form.submit(
-      '/post',
+      "/post",
       {},
       (status, response) => {
         console.log(status, response);
@@ -95,22 +88,71 @@ const CollectForm = () => {
 
   return (
     <>
-    <script type="text/javascript" src="https://js.verygoodvault.com/vgs-collect/2.18.2/vgs-collect.js"></script>
-    <form onSubmit={handleSubmit} className="form">
-      <div className="cc-amount-money">
-        <div id="cc-amount-money" className="form-field"></div>
-      </div>
-      
-      <div id="cc-holder" className="form-field"></div>
-      <div id="cc-number" className="form-field"></div>
-      <div className="form-field-group">
-        <div id="cc-expiration-date" className="form-field"></div>
-        <div id="cc-cvc" className="form-field"></div>
-      </div>
-      <button type="submit" className="form-button">Submit</button>
-    </form>
+      <script
+        type="text/javascript"
+        src="https://js.verygoodvault.com/vgs-collect/2.18.2/vgs-collect.js"
+      />
+      <form
+        onSubmit={handleSubmit}
+        className="form flex flex-col justify-center items-center gap-y-4"
+      >
+        <div className="flex flex-row items-end">
+          <p className="text-3xl md:text-5xl mb-2">$</p>
+          <input
+            placeholder="0.00"
+            className="ml-2 text-5xl md:text-7xl outline-none text-center w-full h-fit"
+            style={{ width: Math.min(Math.max(amount.length, 4), 50) + "ch" }}
+            value={amount}
+            maxLength={7}
+            onChange={(event) => {
+              setAmount(event.target.value);
+            }}
+          />
+          {/* <Input
+            classNames={{
+              input:
+                "text-5xl md:text-7xl outline-none text-center w-full h-fit",
+            }}
+            value={amount}
+            variant="unstyled"
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+          /> */}
+        </div>
+        <Input.Wrapper id="cardHolder" label={"Card Holder"} required>
+          <div
+            id="cc-holder"
+            className=" mt-2 h-9 border rounded border-[#ced4da] px-3 font-poppins"
+          />
+        </Input.Wrapper>
+        <Input.Wrapper id="cardNumber" label={"Card Number"} required>
+          <div
+            id="cc-number"
+            className=" mt-2 h-9 border rounded border-[#ced4da] px-3 font-poppins"
+          />
+        </Input.Wrapper>
+        <Input.Wrapper id="cvc" label={"CVC"} required>
+          <div
+            id="cc-cvc"
+            className=" mt-2 h-9 border rounded border-[#ced4da] px-3 font-poppins"
+          />
+        </Input.Wrapper>
+        <Input.Wrapper id="expiration" label={"Expiration"} required>
+          <div
+            id="cc-expiration-date"
+            className=" mt-2 h-9 border rounded border-[#ced4da] px-3 font-poppins"
+          />
+        </Input.Wrapper>
+        <button
+          type="submit"
+          className="form-buttom bg-blue-500 mt-4 border rounded-lg px-4 py-2 text-white hover:bg-blue-700 transition hover:border-white"
+        >
+          Donate
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default CollectForm;
--- src/pages/InputCreditDrawer.tsx
+++ src/pages/InputCreditDrawer.tsx
@@ -1,27 +1,33 @@
-import { useState } from 'react';
-import { Drawer, Button, Group } from '@mantine/core';
-import CollectForm from './InputCredit'
+import { useState } from "react";
+import { Drawer, Button, Group } from "@mantine/core";
+import CollectForm from "./InputCredit";
 
 export default function InputCreditDrawer() {
   const [opened, setOpened] = useState(false);
 
   return (
     <>
-    <div className="min-h-screen">
-      <Drawer
-        opened={opened}
-        onClose={() => setOpened(false)}
-        title="Register"
-        padding="xl"
-        size="xl"
-      >
-        {<CollectForm></CollectForm>}
-      </Drawer>
+      <div className="min-h-screen">
+        <Drawer
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Donate"
+          padding="xl"
+          size="xl"
+          position="bottom"
+          classNames={{
+            drawer: "h-[75vh]",
+          }}
+        >
+          {<CollectForm />}
+        </Drawer>
 
-      <Group position="center">
-        <Button className='bg-blue-500' onClick={() => setOpened(true)}>Donate</Button>
-      </Group>
-    </div>
+        <Group position="center">
+          <Button className="bg-blue-500" onClick={() => setOpened(true)}>
+            Donate
+          </Button>
+        </Group>
+      </div>
     </>
   );
 }
